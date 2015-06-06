cbeeae71da505a821b8efbecd31eb51aea113d12
mambax7 <mambax7@gmail.com>
2015-06-06 07:00:50 -0400

    Original files

---
+++ aph002/scripts/aph002preloads.js
@@ -0,0 +1,102 @@
+<!--//
+
+//pre loader
+
+newimage0 = new Image();
+
+newimage0.src = "<{xoImgUrl visual/APH002bhv_06.jpg}>";
+
+newimage1 = new Image();
+
+newimage1.src = "<{xoImgUrl visual/APH002bhv_05.jpg}>";
+
+newimage2 = new Image();
+
+newimage2.src = "<{xoImgUrl visual/APH002bhv_04.jpg}>";
+
+newimage3 = new Image();
+
+newimage3.src = "<{xoImgUrl visual/APH002bhv_03.jpg}>";
+
+newimage4 = new Image();
+
+newimage4.src = "<{xoImgUrl visual/APH002bhv_02.jpg}>";
+
+newimage5 = new Image();
+
+newimage5.src = "<{xoImgUrl visual/APH002ghv_06.jpg}>";
+
+newimage6 = new Image();
+
+newimage6.src = "<{xoImgUrl visual/APH002ghv_05.jpg}>";
+
+newimage7 = new Image();
+
+newimage7.src = "<{xoImgUrl visual/APH002ghv_04.jpg}>";
+
+newimage8 = new Image();
+
+newimage8.src = "<{xoImgUrl visual/APH002ghv_03.jpg}>";
+
+newimage9 = new Image();
+
+newimage9.src = "<{xoImgUrl visual/APH002ghv_02.jpg}>";
+
+newimage10 = new Image();
+
+newimage10.src = "<{xoImgUrl visual/APH002rhv_06.jpg}>";
+
+newimage11 = new Image();
+
+newimage11.src = "<{xoImgUrl visual/APH002rhv_05.jpg}>";
+
+newimage12 = new Image();
+
+newimage12.src = "<{xoImgUrl visual/APH002rhv_04.jpg}>";
+
+newimage13 = new Image();
+
+newimage13.src = "<{xoImgUrl visual/APH002rhv_03.jpg}>";
+
+newimage14 = new Image();
+
+newimage14.src = "<{xoImgUrl visual/APH002rhv_02.jpg}>";
+
+newimage15 = new Image();
+
+newimage15.src = "<{xoImgUrl visual/APH002hv_06.jpg}>";
+
+newimage16 = new Image();
+
+newimage16.src = "<{xoImgUrl visual/APH002hv_05.jpg}>";
+
+newimage17 = new Image();
+
+newimage17.src = "<{xoImgUrl visual/APH002hv_04.jpg}>";
+
+newimage18 = new Image();
+
+newimage18.src = "<{xoImgUrl visual/APH002hv_03.jpg}>";
+
+newimage19 = new Image();
+
+newimage19.src = "<{xoImgUrl visual/APH002hv_02.jpg}>";
+
+newimage20 = new Image();
+
+newimage20.src = "<{xoImgUrl visual/APH002gy_08.jpg}>";
+
+newimage21 = new Image();
+
+newimage21.src = "<{xoImgUrl visual/APH002gr_08.jpg}>";
+
+newimage22 = new Image();
+
+newimage22.src = "<{xoImgUrl visual/APH002rd_08.jpg}>";
+
+newimage23 = new Image();
+
+newimage23.src = "<{xoImgUrl visual/APH002_08.jpg}>";
+
+
+//-->
